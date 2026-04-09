60d894a0d8031201ca623a96adfec6c633ec897e
nischalbhattarai509-del <nischalbhattarai509@gmail.com>
2026-04-09 18:24:47 -0500

    Create 002bank_account_oop.py

---
+++ 002bank_account_oop.py
@@ -0,0 +1,25 @@
+class Bank_account:
+    account = "checking"
+
+    def __init__(self, account_owner, balance):
+        self.owner = account_owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("Insufficient balance")
+
+    def display_balance(self):
+        print(self.owner, "balance", self.balance)
+
+account1 = Bank_account("tom", 50000)
+account2 = Bank_account("Bob", 500)
+
+account1.deposit(2000)
+account1.withdraw(300)
+account1.display_balance()
